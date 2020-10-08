363184557eea431304c0093587e5d3438512b126
TJ Foster <tjfoster@TJs-MacBook-Air.local>
2020-10-08 18:30:12 -0400

    first commit

--- app.js
+++ app.js
@@ -18,12 +18,14 @@ const commentRoutes     = require('./routes/comments'),
       indexRoutes       = require('./routes/index');
 
 
+
 // mongoose.connect('mongodb://localhost:27017/yelp_camp_final', {
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
 // })
 // .then(() => console.log('Connected to DB!'))
 // .catch(error => console.log(error.message));
+
 mongoose.connect('mongodb+srv://webdevcamp:lupin@cluster0.agdoz.mongodb.net/yelpcamp666?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
